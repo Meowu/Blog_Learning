2bd6ef2403fa23c4c7a162cfdaf0f734ddec16a3
Meowu <474384902@qq.com>
2018-01-08 23:58:32 +0800

    add category controllers

--- controllers/CategoryControll.js
+++ controllers/CategoryControll.js
@@ -1,5 +1,6 @@
 const {equals, isEmpty, trim, escape} = require('validator')
-const Category = require('../models/Category.js')
+const Category = require('../models/Category')
+const Article = require('../models/Article')
 const {return0, return1, return2, return3} = require('./_response')
 
 exports.addCategory = (req, res, next) => {
@@ -11,13 +12,56 @@ exports.addCategory = (req, res, next) => {
     return1('name不能为空。', res)
   }
   const newCategory = new Category({name: name})
-  console.log(newCategory)
   newCategory.save(function (err) {
     if (err) {
       return3(res)
     } else {
       return0({}, res)
+    }
+  })
+}
+exports.deleteCategory = (req, res, next) => {
+  const id = escape(trim(req.body.id))
+  if (!id) {
+    return1('id不能为空')
+  }
+  Category.findById(id, function(err, result) {
+    if (err) return3(res)
+    if (!result) {
+      return1('id不存在')
+    }
+  })
+  Article.find({'category': id}, function(err, result) {
+    if (err) return3(res)
+    if (result) {
+      return1(`该分类下有 ${result.length} 篇文章，不能删除。`)
+    } else {
+      Category.findByIdAndRemove(id, function(err) {
+        if (err) return3(res)
+        return0({}, res)
+      })
+    }
+  })
+}
 
+exports.getCategories = (req, res, next) => {
+  Category.find({}, function(err, result) {
+    if (err) return3(res)
+    return0(result, res)
+  })
+}
+
+exports.getCategoryArticles = (req, res, next) => {
+  const id = escape(trim(req.params.id))
+  let { page, page_size } = req.query
+  page = Number(page) || 1
+  page_size = Number(page_size) || 10
+  Category.findById(id).populate('articles').exec(function(err, result) {
+    if (err) return3(res)
+    if (!result) {
+      return1('id不存在', res)
+    } else {
+      return0(result, res)
     }
   })
 }
--- routes/CategoryRouter.js
+++ routes/CategoryRouter.js
@@ -1,7 +1,11 @@
+
 const router = require('express').Router()
 
-const { addCategory } = require('../controllers/CategoryControll')
+const { addCategory, getCategories, deleteCategory, getCategoryArticles } = require('../controllers/CategoryControll')
 
 router.post('/new', addCategory)
+router.get('/', getCategories)
+router.delete('/:id', deleteCategory)
+router.get('/:id/articles', getCategoryArticles)
 
 module.exports = router
